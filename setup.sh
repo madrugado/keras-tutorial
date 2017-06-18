008976b641e159cd07750ceb9a9a24ac8aa56235
Valentin Malykh <madrugado@yandex.ru>
2017-06-18 17:53:06 +0300

    NEW: data download & preprocessing

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+pip install -r requirements.txt 
+
+wget https://www.dropbox.com/s/fnpq3z4bcnoktiv/positive.csv?dl=1
+mv positive.csv?dl=1 data/positive.csv
+
+wget https://www.dropbox.com/s/r6u59ljhhjdg6j0/negative.csv?dl=1
+mv negative.csv?dl=1 data/negative.csv
